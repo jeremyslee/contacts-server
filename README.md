ff86ab5a11ba84ebd8e3c2e3792bd91d370cc7d2
Jeremy S. Lee <jeremysanglee@gmail.com>
2018-01-27 00:48:40 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Server for Contacts Project
+
+This is a backend server for the Contacts app.
+
+## Getting Set Up
+
+Getting the server running on your local machine takes only a few steps:
+
+1. clone the project - `git clone https://github.com/jeremyslee/contacts-server.git`
+2. install its dependencies - `npm install`
+3. start the server - `node server.js`
---
+++ contacts.js
@@ -0,0 +1,58 @@
+const clone = require('clone')
+const config = require('./config')
+
+const db = {}
+
+const defaultData = {
+  contacts: [
+    {
+      id: 'brutus',
+      name: 'Brutus Burrito',
+      email: 'brutus@brutusburrito.com',
+      avatarURL: config.origin + '/brutus.jpg'
+    },
+    {
+      id: 'smudge',
+      name: 'Purrfect Smudge',
+      email: 'smudge@purrfectsmudge.com',
+      avatarURL: config.origin + '/smudge.jpg'
+    }
+  ]
+}
+
+const get = (token) => {
+  let data = db[token]
+
+  if (data == null) {
+    data = db[token] = clone(defaultData)
+  }
+
+  return data
+}
+
+const add = (token, contact) => {
+  if (!contact.id) {
+    contact.id = Math.random().toString(36).substr(-8)
+  }
+
+  get(token).contacts.push(contact)
+
+  return contact
+}
+
+const remove = (token, id) => {
+  const data = get(token)
+  const contact = data.contacts.find(c => c.id === id)
+
+  if (contact) {
+    data.contacts = data.contacts.filter(c => c !== contact)
+  }
+
+  return { contact }
+}
+
+module.exports = {
+  get,
+  add,
+  remove
+}
